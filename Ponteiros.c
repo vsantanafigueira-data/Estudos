9593a9d8d4a62ddeb2c42c386f55a53a60d0a714
victor <vsantanafigueira@gmail.com>
2025-10-20 02:20:51 -0300

    parte inicial - ponteiros

--- Ponteiros.c
+++ Ponteiros.c
@@ -1,9 +1,21 @@
+//funcionamento de ponteiros em C
 #include <stdio.h>
 
 int main(){
 
 
-    PRINT("Hello, World!\n");
+    int var = 10;
+    int *ptr = &var;
+
+    printf("valor de var: %d\n", var);
+    printf("endereco de var: %p\n", &var);
+    printf("endereco de ptr: %p\n", ptr);
+    printf("valor apontado por ptr: %d\n", *ptr);
+
+    //printf do endereço na memória de var: %p -> &var
+    //print do endereço apontado por ptr: %p -> ptr
+    //print do valor apontado por ptr: %d -> *ptr
+
 
     return 0;
 }
